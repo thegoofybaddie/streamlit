8097308ebd2d14219abb8842aaec5e74a870d839
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-03-16 23:52:15 +0700

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,6 +1,10 @@
-import streamlit
+import streamlit as st
+import pickle
 
 st.title('Revenue Prediction')
-a = st.number_input('Tham số a')
-b = st.number_input('Tham số b')
-if st.button('Giải'):
+a = st.number_input('Revenue Prediction')
+if st.button('Predict'):
+  filename = 'model.pickle'
+  pickle.dump(model, open(filename, "wb"))
+  model = pickle.load(open(filename, "rb")) 
+  
