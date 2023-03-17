d9811c37b5571e2d77d1ef6fc22f873cf76a85be
QuynhAn <124511413+thegoofybaddie@users.noreply.github.com>
2023-03-17 21:32:25 +0700

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -7,5 +7,5 @@ x = st.number_input('Input Temperature')
 filename = 'model.pickle'
 model = pickle.load(open(filename, "rb"))
 if st.button('Predict'):
-   model.predict(x_test)
+   result = model.predict(x)
   
